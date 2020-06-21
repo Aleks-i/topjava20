1af95c7aa7b838c3eba29824a18af6283958a280
Aleks-i <i.vanovaleksval@gmail.com>
2020-06-22 00:27:35 +0500

    Apply HW3 without Optionale

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM user_meal;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO user_meal (DATE_TIME, DESCRIPTION, CALORIES, USER_ID) VALUES
+('2020-06-20 10:00:00', 'завтрак', 500, 100000),
+('2020-06-20 13:00:00', 'обед', 1000, 100000),
+('2020-06-20 20:00:00', 'ужин', 500, 100000),
+('2020-06-21 10:00:00', 'завтрак', 500, 100000),
+('2020-06-21 13:00:00', 'обед', 1000, 100000),
+('2020-06-21 20:00:00', 'ужин', 500, 100000),
+('2020-06-22 14:00:00', 'Админ ланч', 510, 100001),
+('2020-06-22 21:00:00', 'Админ ужин', 1500, 100001);
